7d73db1e251d4d3e8250383cbfab904a24aeb562
braju007 <erbrj7@gmail.com>
2025-08-03 11:10:45 +0530

    Add array filter method polyfill

---
+++ Polyfills/Filter.js
@@ -0,0 +1,22 @@
+// array method filter polyfill => filter()
+
+// Array.filter((num, i, arr) => {});
+
+Array.prototype.myFilter = function (callback) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      temp.push(this[i]);
+    }
+  }
+  return temp;
+};
+
+const nums = [1, 2, 3, 4];
+
+const divisibleByTwo = nums.myFilter((num, i, arr) => {
+  return num % 2 === 0;
+});
+
+console.log(divisibleByTwo);
+// [ 3, 6, 9, 12 ]
